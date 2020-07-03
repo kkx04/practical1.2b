8f9f6eda142ff0467e5557574d1cc60ee2f69078
kkx04 <kohkx-wm19@student.tarc.edu.m>
2020-07-03 20:51:59 +0800

    commit by kexin

--- app/src/main/java/com/example/android/practical2_parta/MainActivity.java
+++ app/src/main/java/com/example/android/practical2_parta/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.practical2_parta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -18,14 +19,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showToast(View view){
-        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this,"Koh Ke Xin 19WMD10772",Toast.LENGTH_SHORT);
 
         toast.show();
     }
 
     public void countUp(View view){
         ++mCount;
-        if(mShowCount != null)
+        if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
     }
+
+    public void resetCount(View view){
+        mCount=0;
+        mShowCount.setText(String.valueOf(mCount));
+        if(mShowCount !=null) {
+            mShowCount.setText(Integer.toString(mCount));
+            int number = mCount % 2;
+            if (number ==0){
+                view.setBackgroundColor(Color.GREEN);
+            }
+            else
+                view.setBackgroundColor(Color.RED);
+            findViewById(R.id.button_zero).setBackgroundColor(Color.RED);
+        }
+    };
 }
